1e29f0c4b44b656df726b6787ac2ca6117246d3a
Yagi Anggar Prahara <yagi@wego.com>
2015-09-08 18:17:04 +0700

    reformat code

--- src/main/java/com/yagi/App.java
+++ src/main/java/com/yagi/App.java
@@ -8,15 +8,14 @@ import org.joda.time.format.DateTimeFormatter;
  * Simple App to test IANA Timezone converted using JODA Library
  *
  */
-public class App 
-{
+public class App {
 	private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormat
 			.forPattern("yyyy-MM-dd HH:mm");
-	
+
 	private String ianaTimeZone;
 	private String testedDate;
-	
-    public String getIanaTimeZone() {
+
+	public String getIanaTimeZone() {
 		return ianaTimeZone;
 	}
 
@@ -32,103 +31,154 @@ public class App
 		this.testedDate = testedDate;
 	}
 
-	public static void main( String[] args )
-    {
+	public static void main(String[] args) {
 		App app = new App();
-		
+
 		// Set IANA Timezone for Australia/Sydney
 		app.setIanaTimeZone("Australia/Sydney");
-		System.out.println( "Location : " + app.getIanaTimeZone());
-        DateTimeZone sydZone = DateTimeZone.forID(app.getIanaTimeZone());
-        
-        app.setTestedDate("2015-10-04 20:00");
-        System.out.println("Date of "+ app.getTestedDate() +" : " + TIME_FORMATTER.parseDateTime(app.getTestedDate()).withZone(sydZone));
-        
-        app.setTestedDate("2015-09-30 20:00");
-        System.out.println("Date of "+ app.getTestedDate() +" : " + TIME_FORMATTER.parseDateTime(app.getTestedDate()).withZone(sydZone));
-        
-        app.setTestedDate("2016-10-02 20:00");
-        System.out.println("Date of "+ app.getTestedDate() +" : " + TIME_FORMATTER.parseDateTime(app.getTestedDate()).withZone(sydZone));
-        
-        System.out.println("\n ======= \n");
-        
-        /*
-         * IANA Timezone for Australia/Perth
-         */
+		System.out.println("Location : " + app.getIanaTimeZone());
+		DateTimeZone sydZone = DateTimeZone.forID(app.getIanaTimeZone());
+
+		app.setTestedDate("2015-10-04 20:00");
+		System.out.println("Date of "
+				+ app.getTestedDate()
+				+ " : "
+				+ TIME_FORMATTER.parseDateTime(app.getTestedDate()).withZone(
+						sydZone));
+
+		app.setTestedDate("2015-09-30 20:00");
+		System.out.println("Date of "
+				+ app.getTestedDate()
+				+ " : "
+				+ TIME_FORMATTER.parseDateTime(app.getTestedDate()).withZone(
+						sydZone));
+
+		app.setTestedDate("2016-10-02 20:00");
+		System.out.println("Date of "
+				+ app.getTestedDate()
+				+ " : "
+				+ TIME_FORMATTER.parseDateTime(app.getTestedDate()).withZone(
+						sydZone));
+
+		System.out.println("\n ======= \n");
+
+		/*
+		 * IANA Timezone for Australia/Perth
+		 */
 		// Set IANA Timezone for Australia/Perth
 		app.setIanaTimeZone("Australia/Perth");
-        System.out.println( "Location : " + app.getIanaTimeZone());
-        DateTimeZone perthZone = DateTimeZone.forID(app.getIanaTimeZone());
-        
-        app.setTestedDate("2015-10-04 20:00");
-        System.out.println("Date of "+ app.getTestedDate() +" : " + TIME_FORMATTER.parseDateTime(app.getTestedDate()).withZone(perthZone));
-        
-        app.setTestedDate("2016-04-01 20:00");
-        System.out.println("Date of "+ app.getTestedDate() +" : " + TIME_FORMATTER.parseDateTime(app.getTestedDate()).withZone(perthZone));
-        
-        System.out.println("\n ======= \n");
-        
-        /*
-         * IANA Timezone for Australia/Melbourne
-         */
+		System.out.println("Location : " + app.getIanaTimeZone());
+		DateTimeZone perthZone = DateTimeZone.forID(app.getIanaTimeZone());
+
+		app.setTestedDate("2015-10-04 20:00");
+		System.out.println("Date of "
+				+ app.getTestedDate()
+				+ " : "
+				+ TIME_FORMATTER.parseDateTime(app.getTestedDate()).withZone(
+						perthZone));
+
+		app.setTestedDate("2016-04-01 20:00");
+		System.out.println("Date of "
+				+ app.getTestedDate()
+				+ " : "
+				+ TIME_FORMATTER.parseDateTime(app.getTestedDate()).withZone(
+						perthZone));
+
+		System.out.println("\n ======= \n");
+
+		/*
+		 * IANA Timezone for Australia/Melbourne
+		 */
 		// Set IANA Timezone for Australia/Melbourne
 		app.setIanaTimeZone("Australia/Melbourne");
-        System.out.println( "Location : " + app.getIanaTimeZone());
-        DateTimeZone melbourneZone = DateTimeZone.forID(app.getIanaTimeZone());
-        
-        app.setTestedDate("2016-04-01 20:00");
-        System.out.println("Date of "+ app.getTestedDate() +" : " + TIME_FORMATTER.parseDateTime(app.getTestedDate()).withZone(melbourneZone));
-        
-        app.setTestedDate("2015-10-03 20:00");
-        System.out.println("Date of "+ app.getTestedDate() +" : " + TIME_FORMATTER.parseDateTime(app.getTestedDate()).withZone(melbourneZone));
-        
-        System.out.println("\n ======= \n");
-        
-        /*
-         * IANA Timezone for Asia/Novosibirsk
-         */
+		System.out.println("Location : " + app.getIanaTimeZone());
+		DateTimeZone melbourneZone = DateTimeZone.forID(app.getIanaTimeZone());
+
+		app.setTestedDate("2016-04-01 20:00");
+		System.out.println("Date of "
+				+ app.getTestedDate()
+				+ " : "
+				+ TIME_FORMATTER.parseDateTime(app.getTestedDate()).withZone(
+						melbourneZone));
+
+		app.setTestedDate("2015-10-03 20:00");
+		System.out.println("Date of "
+				+ app.getTestedDate()
+				+ " : "
+				+ TIME_FORMATTER.parseDateTime(app.getTestedDate()).withZone(
+						melbourneZone));
+
+		System.out.println("\n ======= \n");
+
+		/*
+		 * IANA Timezone for Asia/Novosibirsk
+		 */
 		// Set IANA Timezone for Asia/Novosibirsk
 		app.setIanaTimeZone("Asia/Novosibirsk");
-        System.out.println( "Location : " + app.getIanaTimeZone());
-        DateTimeZone novoZone = DateTimeZone.forID(app.getIanaTimeZone());
-        
-        app.setTestedDate("2016-03-01 20:00");
-        System.out.println("Date of " + app.getTestedDate() + " : " + TIME_FORMATTER.parseDateTime(app.getTestedDate()).withZone(novoZone));
-        
-        app.setTestedDate("2016-04-15 20:00");
-        System.out.println("Date of "+ app.getTestedDate() +" : " + TIME_FORMATTER.parseDateTime(app.getTestedDate()).withZone(novoZone));
-        
-        System.out.println("\n ======= \n");
-        
-        /*
-         * IANA Timezone for America/Chicago
-         */
+		System.out.println("Location : " + app.getIanaTimeZone());
+		DateTimeZone novoZone = DateTimeZone.forID(app.getIanaTimeZone());
+
+		app.setTestedDate("2016-03-01 20:00");
+		System.out.println("Date of "
+				+ app.getTestedDate()
+				+ " : "
+				+ TIME_FORMATTER.parseDateTime(app.getTestedDate()).withZone(
+						novoZone));
+
+		app.setTestedDate("2016-04-15 20:00");
+		System.out.println("Date of "
+				+ app.getTestedDate()
+				+ " : "
+				+ TIME_FORMATTER.parseDateTime(app.getTestedDate()).withZone(
+						novoZone));
+
+		System.out.println("\n ======= \n");
+
+		/*
+		 * IANA Timezone for America/Chicago
+		 */
 		// Set IANA Timezone for America/Chicago
 		app.setIanaTimeZone("America/Chicago");
-        System.out.println( "Location : " + app.getIanaTimeZone());
-        DateTimeZone chicagoZone = DateTimeZone.forID(app.getIanaTimeZone());
-        
-        app.setTestedDate("2016-03-13 20:00");
-        System.out.println("Date of "+ app.getTestedDate() +" : " + TIME_FORMATTER.parseDateTime(app.getTestedDate()).withZone(chicagoZone));
-        
-        app.setTestedDate("2016-03-12 20:00");
-        System.out.println("Date of "+ app.getTestedDate() +" : " + TIME_FORMATTER.parseDateTime(app.getTestedDate()).withZone(chicagoZone));
-        
-        System.out.println("\n ======= \n");
-        
-        /*
-         * IANA Timezone for America/Indiana/Indianapolis
-         */
+		System.out.println("Location : " + app.getIanaTimeZone());
+		DateTimeZone chicagoZone = DateTimeZone.forID(app.getIanaTimeZone());
+
+		app.setTestedDate("2016-03-13 20:00");
+		System.out.println("Date of "
+				+ app.getTestedDate()
+				+ " : "
+				+ TIME_FORMATTER.parseDateTime(app.getTestedDate()).withZone(
+						chicagoZone));
+
+		app.setTestedDate("2016-03-12 20:00");
+		System.out.println("Date of "
+				+ app.getTestedDate()
+				+ " : "
+				+ TIME_FORMATTER.parseDateTime(app.getTestedDate()).withZone(
+						chicagoZone));
+
+		System.out.println("\n ======= \n");
+
+		/*
+		 * IANA Timezone for America/Indiana/Indianapolis
+		 */
 		// Set IANA Timezone for America/Indiana/Indianapolis
 		app.setIanaTimeZone("America/Indiana/Indianapolis");
-        System.out.println( "Location : " + app.getIanaTimeZone());
-        DateTimeZone indianaZone = DateTimeZone.forID(app.getIanaTimeZone());
-        
-        app.setTestedDate("2016-03-13 20:00");
-        System.out.println("Date of "+ app.getTestedDate() +" : " + TIME_FORMATTER.parseDateTime(app.getTestedDate()).withZone(indianaZone));
-        
-        app.setTestedDate("2016-03-12 20:00");
-        System.out.println("Date of "+ app.getTestedDate() +" : " + TIME_FORMATTER.parseDateTime(app.getTestedDate()).withZone(indianaZone));
-        
-    }
+		System.out.println("Location : " + app.getIanaTimeZone());
+		DateTimeZone indianaZone = DateTimeZone.forID(app.getIanaTimeZone());
+
+		app.setTestedDate("2016-03-13 20:00");
+		System.out.println("Date of "
+				+ app.getTestedDate()
+				+ " : "
+				+ TIME_FORMATTER.parseDateTime(app.getTestedDate()).withZone(
+						indianaZone));
+
+		app.setTestedDate("2016-03-12 20:00");
+		System.out.println("Date of "
+				+ app.getTestedDate()
+				+ " : "
+				+ TIME_FORMATTER.parseDateTime(app.getTestedDate()).withZone(
+						indianaZone));
+
+	}
 }
